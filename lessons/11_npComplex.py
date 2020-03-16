f485ff9c02d648b233e1216fec1e9b3be6bdb10a
mjadiletta <mjadiletta@wpi.edu>
2020-03-16 16:35:02 -0400

    lecture 11

--- lessons/11_npComplex.py
+++ lessons/11_npComplex.py
@@ -1,7 +1,19 @@
 """
 Practice Group 11: Using Numpy for matrix multiplication
 Created: 3/16/2020
+Adopted from Prof. Whitehill CS 453x HW1 and CS/DS 541
 
+Matrix multiplication is a fundamental component to all Machine Learning Algorithms
+and most problems in general. We explored numpy in lecture 6 previously. In this
+lecture, we will explore numpy matrix multiplication.
+
+learning objectives:
+1) matrix addition and subtraction
+2) matrix multiplication and inverses
+3) creating matrices in numpy
+4) transposing matrices in numpy
+5) accessing elements of multidimensional arrays
+6) summing across rows of 2D arrays to create a 1D array
 
 Example 1 is completed as a template.
 For each of the following examples, replace blank lines
@@ -10,25 +22,169 @@ will print "Correct Solution" to the terminal.
 """
 
 import numpy as np
-import copy
 from solutions.CheckSolutions import CheckSolutions
 
 checkSolutions = CheckSolutions().numpyComplexSolutions
-randInt = lambda : np.random.randint(-100, 100)
 
-a = ["test", " ", "this"]
+a = np.array([[1, 2, 3], [6, 3, 2], [9, 9, 9]])  # 3x3
+b = np.array([[2, 5, 3], [4, 4, 2], [1, 2, 3]])  # 3x3
+c = np.array([[1, 2, 1], [4, 5, 5], [6, 7, 8]])  # 3x3
+d = np.array([[1, 2, 3]])  # 1x3
+cv1 = np.array([1, 2, 3])  # 3,
+cv2 = np.array([4, 5, 6])  # 3,
 
 '''
 Example 1: 
-    Code a loop that creates a string using the values from list a
-    Then return the string.
+    return the shape of a numpy array
 '''
-def ex1(list_a):
-    string = ""
-    for element in list_a:
-        string += element
-    return string
+def ex1(array):
+    return np.shape(array)
 
-# make a copy of a, not needed for this problem but in others is necessary
-print(ex1(copy.deepcopy(a)))
-checkSolutions['example1'](ex1, copy.deepcopy(a))
+checkSolutions['example1'](ex1, a)
+
+
+
+'''
+Example 2: 
+    return the sum of two arrays if their dimensions are the same. 
+    if they are not the same, then do not sum them and return -1
+'''
+def ex2(a, b):
+    if None:
+        return None
+    else:
+        return None
+
+checkSolutions['example2'](ex2, a, b)
+
+
+
+'''
+Example 3: 
+    return the element-wise product of two arrays (a and b) and add the transpose of a third array (c)
+    only do this if all their dimensions are the same. 
+    if the dimensions are not the same, then do not sum them and return -1. 
+'''
+def ex3(a, b, c):
+    if None:
+        return None
+    else:
+        return None
+
+checkSolutions['example3'](ex3, a, b, c)
+checkSolutions['example3'](ex3, a, b, d)
+
+
+
+'''
+Example 4: 
+    Given two column vectors (a, b) - a column vector is a 1 dimensional array
+    compute the inner product, ie (dot product)
+'''
+def ex4(a, b):
+    return None
+
+checkSolutions['example4'](ex4, cv1, cv2)
+
+
+
+'''
+Example 5: 
+    Given a matrix A, return a matrix of all zeros with the same dimensions.
+    Hint: look up numpy.zeros 
+'''
+def ex5(A):
+    return None
+
+checkSolutions['example5'](ex5, a)
+
+
+
+'''
+Example 6: 
+    Given a 2D matrix A, return a vector of all one of length (num rows in A).
+    Hint: look up numpy.ones 
+'''
+def ex6(A):
+    return None
+
+checkSolutions['example6'](ex6, a)
+
+
+
+'''
+Example 7: 
+    Given a square matrix A and a scalar alpha, 
+    return A + alpha*I where I is the identity matrix of shape A
+    Hint: look up numpy.eye
+'''
+def ex7(A, alpha):
+    return None
+
+checkSolutions['example7'](ex7, a, 5)
+
+
+
+'''
+Example 8: 
+    Given a 2D matrix A 
+    return the i, j where i is the ith row and j is the jth column
+    Hint: look up indexing 2D numpy array
+'''
+def ex8(A, i, j):
+    return None
+
+checkSolutions['example8'](ex8, a, 1, 2)
+
+
+
+'''
+Example 9: 
+    Given a 2D matrix A and integer i, 
+    return the sum of all the entries in row i
+    Note: do not use a loop! Use np.sum()
+'''
+def ex9(A, i):
+    return None
+
+checkSolutions['example9'](ex9, a, 1)
+
+
+
+'''
+Example 10: 
+    Given matrix A and scalars c, d:
+    compute the arithmetic mean over all entries of A that
+    are between c and d (inclusive).
+    
+    ex: a = [[1, 2, 0], [4, 3, 1]]    c = 1       d = 3 
+    ans = (1 + 2 + 3 + 1)/4 = 7/4
+    
+    Note: do not use a loop! 
+    Hint: use np.nonzero and np.mean
+'''
+def ex10(A, c, d):
+
+    return None
+
+checkSolutions['example10'](ex10, a, 1, 3)
+
+
+
+'''
+Example 11: 
+    Given matrix A and column vector x, 
+    use np.linalg.solve to compute A^(-1)x
+    Do not use np.linalg.inv or A**-1 because this is numerically unstable. 
+        It can make a big difference!    
+    
+    read: https://docs.scipy.org/doc/numpy/reference/generated/numpy.linalg.solve.html
+    
+    Hint: 
+        np.linalg.solve(a, b) = c such that a*c = b
+        Note A^(-1)x = m    =>   A*m = x  
+'''
+def ex11(A, x):
+    return None
+
+checkSolutions['example11'](ex11, a, cv1)
--- solutions/numpyComplexSolutions.py
+++ solutions/numpyComplexSolutions.py
@@ -4,9 +4,122 @@ class numpyComplexSolutions:
     def __init__(self):
         self.numpyComplexSol = {}
         self.numpyComplexSol['example1'] = self.ex1
+        self.numpyComplexSol['example2'] = self.ex2
+        self.numpyComplexSol['example3'] = self.ex3
+        self.numpyComplexSol['example4'] = self.ex4
+        self.numpyComplexSol['example5'] = self.ex5
+        self.numpyComplexSol['example6'] = self.ex6
+        self.numpyComplexSol['example7'] = self.ex7
+        self.numpyComplexSol['example8'] = self.ex8
+        self.numpyComplexSol['example9'] = self.ex9
+        self.numpyComplexSol['example10'] = self.ex10
+        self.numpyComplexSol['example11'] = self.ex11
 
     def ex1(self, f, a):
-        if f(a) == (a):
+        if f(a) == np.shape(a):
             print("Example 1: Correct Solution")
         else:
             print("Example 1: Incorrect Solution")
+
+    def ex2(self, f, a, b):
+        try:
+            if np.shape(a) == np.shape(b):
+                sol = a + b
+            else:
+                sol = -1
+            if np.ndarray.tolist(f(a, b)) == np.ndarray.tolist(sol):
+                print("Example 2: Correct Solution")
+            else:
+                print("Example 2: Incorrect Solution")
+        except:
+            try:
+                if sol == f(a, b):
+                    print("Example 2: Correct Solution")
+                else:
+                    print("Example 2: Incorrect Solution")
+            except:
+                print("Example 2: Incorrect Solution")
+
+    def ex3(self, f, a, b, c):
+        if np.shape(a) == np.shape(b) and np.shape(a) == np.shape(c):
+            sol = a * b + c.T
+        else:
+            sol = -1
+        try:
+            if np.ndarray.tolist(f(a, b, c)) == np.ndarray.tolist(sol):
+                print("Example 3: Correct Solution")
+            else:
+                print("Example 3: Incorrect Solution")
+        except:
+            try:
+                if sol == f(a, b, c):
+                    print("Example 3: Correct Solution")
+                else:
+                    print("Example 3: Incorrect Solution")
+            except:
+                print("Example 3: Incorrect Solution")
+
+    def ex4(self, f, a, b):
+        try:
+            if f(a, b) == np.dot(a, b):
+                print("Example 4: Correct Solution")
+            else:
+                print("Example 4: Incorrect Solution")
+        except:
+            print("Example 4: Incorrect Solution")
+
+    def ex5(self, f, a):
+        try:
+            if np.ndarray.tolist(f(a)) == np.ndarray.tolist(np.zeros(np.shape(a))):
+                print("Example 5: Correct Solution")
+            else:
+                print("Example 5: Incorrect Solution")
+        except:
+            print("Example 5: Incorrect Solution")
+
+    def ex6(self, f, a):
+        try:
+            if np.ndarray.tolist(f(a)) == np.ndarray.tolist(np.ones(np.shape(a)[0])):
+                print("Example 6: Correct Solution")
+            else:
+                print("Example 6: Incorrect Solution")
+        except:
+            print("Example 6: Incorrect Solution")
+
+    def ex7(self, f, A, alpha):
+        try:
+            if np.ndarray.tolist(f(A, alpha)) == np.ndarray.tolist((A + alpha * np.eye(np.shape(A)[0]))):
+                print("Example 7: Correct Solution")
+            else:
+                print("Example 7: Incorrect Solution")
+        except:
+            print("Example 7: Incorrect Solution")
+
+    def ex8(self, f, a, i, j):
+        if f(a, i, j) == a[i][j]:
+            print("Example 8: Correct Solution")
+        else:
+            print("Example 8: Incorrect Solution")
+
+    def ex9(self, f, a, i):
+        if f(a, i) == np.sum(a[i]):
+            print("Example 9: Correct Solution")
+        else:
+            print("Example 9: Incorrect Solution")
+
+    def ex10(self, f, a, c, d):
+        intermediate = a[np.nonzero(a >= c)]
+        if f(a, c, d) == np.mean(intermediate[np.nonzero(intermediate<=d)]):
+            print("Example 10: Correct Solution")
+        else:
+            print("Example 10: Incorrect Solution")
+
+    def ex11(self, f, A, x):
+        try:
+            if np.ndarray.tolist(f(A, x)) == np.ndarray.tolist(np.linalg.solve(A, x)):
+                print("Example 11: Correct Solution")
+            else:
+                print("Example 11: Incorrect Solution")
+        except:
+            print("Example 11: Incorrect Solution")
+
