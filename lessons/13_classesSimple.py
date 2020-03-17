caee71412e59356b172254d731ca0f366eb5caec
mjadiletta <mjadiletta@wpi.edu>
2020-03-17 19:17:54 -0400

    small working things

--- lessons/13_classesSimple.py
+++ lessons/13_classesSimple.py
@@ -14,12 +14,12 @@ application class. Using OOP, our code will be easy to read and well organized.
 Note that Python is not the only language that supports OOP. Other languages
 like Java, C++, and JavaScript also support OOP. However, there are other
 programming paradigms, and languages that support them, that we will not cover
-in this lecture series. (Sorry for the block text)
+in this lecture series.
 
 Learning Objectives:
     1. Define a class
     2. Define class attributes
-    3 Define a class method
+    3. Define a class method
     4. Create a constructor method
 
 
@@ -37,8 +37,10 @@ Example 1: Define a class
 
 In this example you will learn how to define a class in Python. Remember that 
 the class is the template that you will use to create objects in your program. 
-So everything must be generalized. We have included an example of a class 
-definition below. Please use this example to create your own class called "Application"
+
+We have included an example of a class definition below. 
+
+Please use this example to create your own class called "Application"
 NOTE: you can just copy the methods/functions from the example class.
 '''
 
@@ -61,15 +63,20 @@ Example 2: Class Attributes
 
 In the previous example you basically had to write one line of code and you 
 copied a couple of functions from the example class. In this example we will 
-introduce the concept of class attributes. An attribute is basically a variable 
+introduce the concept of class attributes. An attribute is a variable 
 that stores data for each object that you create. If you want to apply for a job 
 you might need to put your name, address, date of birth, and some other information.
 Each of these pieces of information may be considered an attribute of the Application 
-class. We have already included the Application class you created in the previous 
+class. 
+
+We have already included the Application class you created in the previous 
 example, with one attribute. Please add the DOB (date of birth - just the year 
 the person was born), SSN (social security number), and position attributes to the 
 Application class. 
-NOTE: these attributes are all defined within this "__init__" function... just accept 
+
+User variable names: dob, ssn, and position for variable names. 
+
+Note: these attributes are all defined within this "__init__" function... just accept 
 this for now, we will talk more about this later.
 '''
 
@@ -80,6 +87,9 @@ class Application:
 
     def submit(self):
         print("Submitted " + self.name + "'s application!")
+
+#checkSolutions['example2'](Application())
+
 
 
 '''
@@ -100,14 +110,19 @@ To call the function you would write "walk(23)" in your code.
 
 Calling a function on an object is very similar, however, you need to be sure that the 
 function you are calling was defined within the class. In the previous example, we 
-defined a class Application, so if we created an object called "app1" based on the Application 
-class we can call the "submit" function with the following code:
-        app1.submit()
+defined a class Application, so if we created an object called "app1": 
+    app1 = Application(var1, var2, ...)
+    
+based on the Application class we can call the "submit" function with the following code:
+    app1.submit()
 
-Ok, that's a lot of information. Hopefully you understood most of that. Please try to add 
-another method to our Application class called "calculate_applicant_age." This function will
-simply calculate the age (in years) of the applicant based on the DOB attribute (the year 
-the applicant was born) and return that age. Assume the current year is 2020.
+Please try to add another method to our Application class called 
+"calculate_applicant_age." This function will calculate the age 
+(in years) of the applicant based on the DOB attribute (the year the applicant was born).
+
+return the age of the person
+
+Note: Assume the current year is 2020.
 '''
 
 
@@ -128,16 +143,18 @@ class Application:
 '''
 Example 4: Class constructor methods
 
-In the previous three examples you have seen this "__init__" method, now you will find out 
-exactly what it means. The "__init__" method is a constructor. This method constructs an 
-object based on the class. When you try to make an object from a class you have defined the 
-"__init__" method will be executed IMMEDIATELY. In Python the __init__ method is used to define 
-class attributes. The inputs to the __init__ method are the data necessary for defining the 
-attributes for that particular object. As you have seen, when you create a new attribute you need 
-to add a new input to your __init__ method definition (e.g., name_in, dob_in, ssn_in, and position_in).
+In the previous three examples you have seen this "__init__" method.
 
-There may be some instances when you don't need any data to be input in order to define an attribute, 
-but don't worry about that right now :)
+The "__init__" method is a constructor. This method constructs an object based on the class. 
+When you make an object from a class, if you have defined the "__init__" method, it 
+will be executed IMMEDIATELY. In Python the __init__ method is used to define class attributes. 
+
+The inputs to the __init__ method are the data necessary for defining the attributes 
+for that particular object. As you have seen, when you create a new attribute you 
+need to add a new input to your __init__ method definition 
+    (e.g., name_in, dob_in, ssn_in, and position_in).
+
+There may even be some instances when you don't need any data to be input to create an object.
 
 Because you have already defined a constructor in the previous examples in this one you will create a 
 new object. Please name the object "app2" and use the definition of "app1" below as a reference. 
